4e4c1edfc3932d171cebdcddf25f799b386ff888
legendofskidmark <42317045+legendofskidmark@users.noreply.github.com>
2019-04-27 02:57:11 +0530

    Create alexnet.py
    DCNN Model - AlexNet

---
+++ alexnet.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+""" AlexNet.
+
+Applying 'Alexnet' to Oxford's 17 Category Flower Dataset classification task.
+
+References:
+    - Alex Krizhevsky, Ilya Sutskever & Geoffrey E. Hinton. ImageNet
+    Classification with Deep Convolutional Neural Networks. NIPS, 2012.
+
+"""
+
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.estimator import regression
+
+
+
+def alexnet(width, height, lr):
+    # Building 'AlexNet'
+    network = input_data(shape=[None, width, height, 1], name = 'input')
+    network = conv_2d(network, 96, 11, strides=4, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 256, 5, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 9, activation='softmax')
+    network = regression(network, optimizer='momentum',
+                         loss='categorical_crossentropy',
+                         learning_rate=lr, name = 'targets')
+
+        
+    # Training
+    
+
+    model = tflearn.DNN(network, checkpoint_path='model_alexnet',tensorboard_verbose=2, tensorboard_dir='log')
+    return model
+
+#tensorboard --logdir=foo:F:/python_GTA/log
